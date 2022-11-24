8b02388a58d7bd86fd43f043c885fb6554697c56
Noh SOOYOUNG <645277@naver.com>
2022-11-24 20:51:55 +0900

    add_camera

--- get_QR_position_Serial.py
+++ get_QR_position_Serial.py
@@ -1,9 +1,11 @@
 import pyzbar.pyzbar as pyzbar
 import cv2
 import serial
+from common.function import onchange
 
 # 비디오캡쳐 열기
-cap = cv2.VideoCapture(0)
+cap_1 = cv2.VideoCapture(0)
+cap_2 = cv2.VideoCapture(1)
 
 # 전역변수 설정
 global QR_Position
@@ -12,13 +14,17 @@ global QR_Position
 x, y, w, h = 0, 0, 0, 0
 
 # 아두이노 시리얼통신 설정
-arduino_port = "COM6"
-arduino_serial = serial.Serial(arduino_port, 115200, timeout=1)
-
+'''
+pi_port = "COM6"
+pi_serial = serial.Serial(pi_port, 115200, timeout=1)
+'''
 while True:
     # 프레임 읽어오기
-    ret, frame = cap.read()
+    ret, frame = cap_1.read()
     if not ret:
+        continue
+    ret_2 , frame_2 = cap_2.read()
+    if not ret_2:
         continue
     # 프레임 그레이스케일로 저장
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -41,10 +47,19 @@ while True:
         break
     # QRcode 중앙좌표 읽어오기
     QR_Position = str([x + (h / 2)])+str([y + (w / 2)])
-    print(QR_Position)  # print the center of the QR code
+    print(QR_Position)  # print the center of the QR code5
     # 아두이노에서 요청이 오면 좌표값 보내주기
-    if arduino_serial.readable():
-        if arduino_serial.readline().decode("utf-8"):
-            arduino_serial.write(QR_Position.encode())
-cap.release()
+    change = onchange(frame)
+    cv2.imshow("test", frame_2)
+    
+    '''
+    if change >= 50:
+        pi_serial.write("stop".encode())
+        print("send to arduino")
+        
+    if pi_serial.readable():
+        if pi_serial.readline().decode("utf-8") == "give me the position":
+            pi_serial.write(QR_Position.encode())
+'''
+cap_1.release()
 cv2.destroyAllWindows()
